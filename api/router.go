6ad8cd6ed17b48d3368084cb3b9f23f6437a281a
qianlizeguo <1723877247@qq.com>
2026-01-06 12:13:43 +0800

    add proxy

--- api/router.go
+++ api/router.go
@@ -1,11 +1,10 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-dev-frame/sponge/pkg/gin/proxy"
 )
 
 var router *gin.Engine
@@ -16,37 +15,35 @@ var authUrl = "https://auth.suno.com"
 // suno
 func init() {
 	router = gin.Default()
-	router.Any("/*path", func(context *gin.Context) {
-		uri := context.Param("path")
-		reqUrl := apiUrl
-		fmt.Println("请求的链接", uri)
-		if after, ok := strings.CutPrefix(uri, "/suno-api"); ok {
-			uri = after
-		} else if after0, ok0 := strings.CutPrefix(uri, "/auth-api"); ok0 {
-			uri = after0
-			reqUrl = authUrl
-		} else {
-			context.String(http.StatusNotFound, "404 Not found")
-			return
-		}
-		url := reqUrl + uri
-		req, err := http.NewRequestWithContext(context, context.Request.Method, url, context.Request.Body)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		req.Header = context.Request.Header
-		req.PostForm = context.Request.PostForm
-		req.Form = context.Request.Form
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			context.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		context.DataFromReader(resp.StatusCode, resp.ContentLength, "application/json", resp.Body, nil)
+	// 初始化代理中间件，默认管理接口挂载在 /endpoints 下
+	p := proxy.New(router)
+
+	// 配置路由规则
+	setupProxyRoutes(p)
+
+	// 你的 Gin 依然可以处理普通路由
+	// router.GET("/ping", func(c *gin.Context) {
+	//     c.JSON(200, gin.H{"message": "我是网关本体"})
+	// })
+}
+
+func setupProxyRoutes(p *proxy.Proxy) {
+	// 规则1：将 /proxy/ 开头的请求分发给集群 A
+	// 默认使用轮询策略，健康检查间隔5秒
+	err := p.Pass("/suno-api/", []string{
+		apiUrl,
 	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 规则2：将 /personal/ 开头的请求分发给集群 B
+	err = p.Pass("/auth-api/", []string{
+		authUrl,
+	})
+	if err != nil {
+		panic(err)
+	}
 }
 
 func Listen(w http.ResponseWriter, r *http.Request) {
